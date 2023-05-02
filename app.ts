f4b0522fdb518039d44c001b419ee87c50f5b4eb
amnioc <nicdhillon19@gmail.com>
2023-05-02 19:30:47 +0100

    preparation for seeding file and initial testing structure

--- app.ts
+++ app.ts
@@ -1,5 +1,8 @@
-const express = require("express");
+// export {};
 
+const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const Comment = require("/home/nico/northcoders/projects/wild-swimming/wild-swimming-be/models/commentModel.ts");
 
 module.exports = app;
